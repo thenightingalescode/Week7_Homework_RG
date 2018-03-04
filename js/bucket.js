61e75863ef31bf88cfedd8dbd6f1461cb124abc8
Remi <Remi.gurak@gmail.com>
2018-03-04 16:51:39 -0500

    Initial commit

---
+++ js/bucket.js
@@ -0,0 +1,77 @@
+$( document ).ready(function() {
+  $( ".leslieButton" ).click(function() {
+  $('.info').hide("fast",function(){
+  });
+  $( '.leslieInfo' ).toggle( "slow", function() {
+  });
+});
+
+$( ".ronButton" ).click(function() {
+  $('.info').hide("fast",function(){
+  });
+  $( '.ronInfo' ).toggle( "slow", function() {
+  });
+});
+
+$( ".aprilButton" ).click(function() {
+  $('.info').hide("fast",function(){
+  });
+  $( '.aprilInfo' ).toggle( "slow", function() {
+  });
+});
+
+$( ".annButton" ).click(function() {
+  $('.info').hide("fast",function(){
+  });
+  $( '.annInfo' ).toggle( "slow", function() {
+  });
+});
+
+$( ".andyButton" ).click(function() {
+  $('.info').hide("fast",function(){
+  });
+  $( '.andyInfo' ).toggle( "slow", function() {
+  });
+});
+
+$( ".tomButton" ).click(function() {
+  $('.info').hide("fast",function(){
+  });
+  $( '.tomInfo' ).toggle( "slow", function() {
+  });
+});
+
+$( ".donnaButton" ).click(function() {
+  $('.info').hide("fast",function(){
+  });
+  $( '.donnaInfo' ).toggle( "slow", function() {
+  });
+});
+
+$( ".benButton" ).click(function() {
+  $('.info').hide("fast",function(){
+  });
+  $( '.benInfo' ).toggle( "slow", function() {
+  });
+});
+
+$( ".chrisButton" ).click(function() {
+  $('.info').hide("fast",function(){
+  });
+  $( '.chrisInfo' ).toggle( "slow", function() {
+  });
+});
+$( ".jerryButton" ).click(function() {
+  $('.info').hide("fast",function(){
+  });
+  $( '.jerryInfo' ).toggle( "slow", function() {
+  });
+});
+
+$( ".highlightsButton" ).click(function() {
+  $( '.highlights' ).toggle( "slow", function() {
+  });
+});
+
+$( "#tabs" ).tabs();
+});
